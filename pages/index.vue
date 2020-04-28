5427b0981541a30da845321730010f63373f1e88
Angelo Calub <angelo.calub@gmail.com>
2020-04-28 10:20:48 -0700

    taking down coding challenge

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,14 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col v-for="(product,index) in products" :key="index" class="d-flex child-flex" cols="12" sm="4">
-        <Product
-          v-on:show-carousel="showCarousel"
-          :productHeroImageSrc="product.heroImageSrc"
-          :id="product.id"
-          :name="product.name"
-          :link="product.link"
-          :reviews="product.reviews"
-          :price="product.prices">
-        </Product>
-        </v-col>
-      </v-row>
-      <v-overlay :value="overlay">
-        <Carousel v-on:close="overlay=false" :productId="productId"/>
-      </v-overlay>
+      <v-col>
+        a
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import { PRODUCTS, PRODUCT_BY_ID } from '@/lib/types';
-import { mapGetters } from 'vuex';
-import Product from '@/components/product';
-import Carousel from '@/components/carousel';
 export default {
-  data() {
-    return {
-      overlay: false,
-      productId: null
-    };
-  },
-  components: {
-    Product,
-    Carousel
-  },
-  computed: {
-    ...mapGetters([PRODUCTS])
-  },
-  methods: {
-    showCarousel(id) {
-      this.productId = id;
-      this.overlay = true;
-    }
-  }
 }
 </script>
